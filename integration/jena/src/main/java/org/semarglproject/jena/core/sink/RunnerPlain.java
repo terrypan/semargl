43c9f183253c546d0389449b7e31a529f2084be0
Terry Panagoulis <epanagoulis@gmail.com>
2013-02-01 12:36:13 +0000

    Commented out RunnerPlain

--- integration/jena/src/main/java/org/semarglproject/jena/core/sink/RunnerPlain.java
+++ integration/jena/src/main/java/org/semarglproject/jena/core/sink/RunnerPlain.java
@@ -1,6 +1,6 @@
 package org.semarglproject.jena.core.sink;
 
-import org.semarglproject.rdf.TurtleSerializer;
+//import org.semarglproject.rdf.TurtleSerializer;
 import org.semarglproject.rdf.rdfa.RdfaParser;
 import org.semarglproject.sink.CharOutputSink;
 import org.semarglproject.source.StreamProcessor;
@@ -13,11 +13,11 @@ public class RunnerPlain {
    
 	 	//Proxy.setProxy();   
 	        
-	        CharOutputSink outputSink = new CharOutputSink();
-	        StreamProcessor sp = new StreamProcessor(RdfaParser.connect(TurtleSerializer.connect(outputSink)));
+	        // CharOutputSink outputSink = new CharOutputSink();
+	        // StreamProcessor sp = new StreamProcessor(RdfaParser.connect(TurtleSerializer.connect(outputSink)));
 
-	        outputSink.connect(System.out);
-	        sp.process("file:/C:/terrypan/testRDFaDocs/input/testCase-nested.html");
+	        // outputSink.connect(System.out);
+	        // sp.process("file:/C:/terrypan/testRDFaDocs/input/testCase-nested.html");
 	 	
 	 	//TripleSink mySink = new LiteralCounterSink();
 	       // StreamProcessor sp = new StreamProcessor(RdfaParser.connect(mySink));
